da9a2756bcc9dea994b7b801bf356525f2384530
Atziri Camacho <atziricamher@gmail.com>
2024-04-24 10:22:27 -0600

    Correccion en la validacion del folio

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 /*  ---------------------------------------------------------------------------------------------------------------------------------- FOLIO DE RESERVACIONES ---------
+
 ------------------------ ESPECIFICACIONES:
 > 'Math.random' genera un decimal aleatorio entre 0 y 1
 > 'Math.floor' convierte en entero el numero anterior */
@@ -9,30 +10,27 @@ function numeroAleatorio(min, max) {
 }
 
 document.getElementById('confirmacion').addEventListener('click',function(){
-  const leyenda = document.getElementById('leyenda'); /* -- ubicar al h1 que contendra la leyenda por medio de una variable --*/
-  leyenda.textContent = 'Su folio de reservación es:'; /* -- asignarle el texto a la variable anterior para que se muestre en pantalla al hacer click --*/
-
-  /* -- Mismo proceso, pero, en vez de enviar un texto, invocamos a la funcion que nos dará el folio -- */
-  const folio = document.getElementById('folio'); 
-  folio.textContent = numeroAleatorio(10000,99999); /* los parametros son el minimo y maximo para generar un numero de 5 digitos*/
-});
-
-
-
-/*  ---------------------------------------------------------------------------------------------------------------------------------- VALIDACIONES ---------*/
-
-function reservaciones() {
   let user = document.getElementById("nombre").value;
   let number = document.getElementById("numero").value;
 
-  if(user == "" || number == ""){
-      swal('¡Ha ocurrido un error!', 'No se admiten espacios en blanco.', 'error');  /* Validar que no estén vacios los input */
+  if(user < 3 && number == ""){
+    /* >'swal' funcion de SweetAlert, 1er campo titulo, 2do campo descripcion, 3er campo iconos que ofrece la liberia */
+    swal('¡Ha ocurrido un error!', 'No se admiten espacios en blanco.', 'error');  /* Validar que no estén vacios los input (min. de user 3; ej. 'Ana') */
   } else if(number.length !== 10){
     swal('¡Información incompleta!', 'Verifique la longitud del número.', 'warning'); /* Validar la longitud del telefono */
   } else {
     swal('¡Operación exitosa!', 'Guarde su comprobante, el no. de folio.', 'success');
-  }
+
+    const leyenda = document.getElementById('leyenda'); /* -- ubicar al h1 que contendra la leyenda por medio de una variable --*/
+    leyenda.textContent = 'Su folio de reservación es:'; /* -- asignarle el texto a la variable anterior para que se muestre en pantalla al hacer click --*/
+
+    /* -- Mismo proceso, pero, en vez de enviar un texto, invocamos a la funcion que nos dará el folio -- */
+    const folio = document.getElementById('folio'); 
+    folio.textContent = numeroAleatorio(10000,99999); /* los parametros son el minimo y maximo para generar un numero de 5 digitos*/
 }
+});
+
+
 
 
 
